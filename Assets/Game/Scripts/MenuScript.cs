c104c29327ca79e7d67700d0e6ada8b030ddd740
Gaston Zarate <gaston.z195@gmail.com>
2020-05-28 17:09:58 -0300

    Modified Menu

---
+++ Assets/Game/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    public void lowQuality()
+    {
+        QualitySettings.SetQualityLevel(0);
+    }
+
+    public void mediumQuality()
+    {
+        QualitySettings.SetQualityLevel(2);
+    }
+
+    public void HighQuality()
+    {
+        QualitySettings.SetQualityLevel(4);
+    }
+
+    public void UltraQuality()
+    {
+        QualitySettings.SetQualityLevel(5);
+    }
+}
