c12e9c605ba495e4ef0d310db631cc3e9a4e2517
Gaston Zarate <gaston.z195@gmail.com>
2020-07-08 16:47:12 -0300

    Updated animals

---
+++ Assets/Game/Scripts/MainScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cat;
+    public float speed = 1.0f;
+    public navMeshAgent;
+    navMeshAgent = GetComponent<NavMeshAgent>();
+
+
+    void Start()
+    {
+
+        navMeshAgent.SetDestination():
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
